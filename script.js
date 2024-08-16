1b58186dfaa40208e8b4dc447dfe5a563b707128
suvakantamalik <134865642+suvakantamalik@users.noreply.github.com>
2024-08-16 21:12:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //your JS code here. If required.
 const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
-let list = document.getElementById("myList");
+let list = document.getElementById("bands");
 for (i = 0; i < bands.length; ++i) {
 	let li = document.createElement('li');
     li.innerText = bands[i];
